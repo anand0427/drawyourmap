5a56eef8fa9bfe1d35388214dc822846374a9fb9
anand0427 <saianand0427@gmail.com>
2020-03-11 18:00:18 +0530

    first commit

--- mapApp/routes.py
+++ mapApp/routes.py
@@ -31,7 +31,7 @@ def getCoordinates(cityNames):
 
 def plotMap(locationList):
 
-    m = folium.Map(location=[20, 0], zoom_start=3)
-    for i in locationList:
-        folium.Marker(i,icon=folium.Icon(color='green', icon='ok')).add_to(m)
-    m.save("build/maps.html")
+    folMap = folium.Map(location=[20, 0], zoom_start=3)
+    for i in len(locationList):
+        folium.Marker(locationList[i],icon=folium.Icon(color='green', icon='ok', popup=locationList[c])).add_to(folMap)
+    folMap.save("maps.html")
